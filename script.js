3bdad9c7fe79a4d88c58eba84ce0a17b3e406eea
Kalande_Kapaya <kjkreed@gmail.com>
2023-02-22 23:19:03 +0200

    Done with CSS and HTML part

---
+++ script.js
@@ -0,0 +1,5 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const main = document.querySelector('.main');
+
+left.addEventListener('mousecenter', () => main.classList)
